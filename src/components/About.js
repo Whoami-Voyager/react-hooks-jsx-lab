fcd3b0c4935db4096f12d8e4317aceda1825e9c5
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-09 17:54:56 -0700

    got it to work

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Colorado is a pretty cool state to live in and I would highly recommend visiting for the sights</p>
+    <img src= {image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
